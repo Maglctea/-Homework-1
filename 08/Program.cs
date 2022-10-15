b8ebedd4b06869bb4f635b8d30a37b276b39bdae
Fortunella <sherbakovdaniil6@gmail.com>
2022-10-15 20:11:55 +0300

    Решено 8 задание

---
+++ 08/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число:");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a < 1)
+    Console.WriteLine("Недопустимое число");
+else
+{
+    int count = 2;
+
+    while (count <= a)
+    {
+        Console.Write($"{count} ");
+        count += 2;
+    }
+}
